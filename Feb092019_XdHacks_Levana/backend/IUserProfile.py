7a303d72b922eb534008ef27411f728c21e5d736
Levana Yang <sachiyukimura@gmail.com>
2019-02-10 08:49:55 -0800

    added a new page for index

--- Feb092019_XdHacks_Levana/backend/IUserProfile.py
+++ Feb092019_XdHacks_Levana/backend/IUserProfile.py
@@ -1,12 +1,13 @@
 # Tests for User Profile functions
+from django.test import TestCase
 from Feb092019_XdHacks_Levana.backend import UserProfile
-import unittest
 
-class TestUserProfile(unittest.TestCase):
+
+class TestUserProfile(TestCase):
+
+    def setUp(self):
+        UserProfile.create("clim", "lyang@cdrd.ca", "wiggy123")
 
     def test_get_username(self):
-        user = UserProfile("clim", "lyang@cdrd.ca", "wiggy123")
-        result = user.get_username()
-
-
-
+        result = UserProfile.AppUser.get_username()
+        self.assertEqual(result, "clim")
