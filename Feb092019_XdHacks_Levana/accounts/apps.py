315de61d9cf960f3f203e09419f618364476165e
myousif9 <m.yousif797@gmail.com>
2019-02-10 01:36:03 -0800

    "fixed log in page"

--- Feb092019_XdHacks_Levana/accounts/apps.py
+++ Feb092019_XdHacks_Levana/accounts/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class LogregConfig(AppConfig):
-    name = 'logreg'
+    name = 'accounts'
--- Feb092019_XdHacks_Levana/accounts/views.py
+++ Feb092019_XdHacks_Levana/accounts/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Template, Context 
 
 def index(request):
-    return HttpResponse("Whats goood my doods")
+    return render(request, 'registration/login.html')
 
 # Create your views here.
