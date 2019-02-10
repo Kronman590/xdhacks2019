2f66df4018fa3e25f92139876f7eeb536f4216e6
myousif9 <m.yousif797@gmail.com>
2019-02-10 08:28:14 -0800

    linking pages and adding html templates

--- Feb092019_XdHacks_Levana/Feb092019_XdHacks_Levana/urls.py
+++ Feb092019_XdHacks_Levana/Feb092019_XdHacks_Levana/urls.py
@@ -20,5 +20,4 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('',TemplateView.as_vie(template_name=))
 ]
